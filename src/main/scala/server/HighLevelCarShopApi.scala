cce594a071299974af04b890759d4ed464ae1d5d
wu-yichen <wuyichen1987@gmail.com>
2021-04-13 21:30:13 +0100

    Use SprayJsonSupport

--- src/main/scala/server/HighLevelCarShopApi.scala
+++ src/main/scala/server/HighLevelCarShopApi.scala
@@ -2,7 +2,7 @@ package server
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
@@ -12,7 +12,6 @@ import model.Car
 import persistence.CarDb
 import persistence.CarDb.{AddCar, FindAllCars, FindCarsById, IsInStock}
 import server.CarShop.dataSetup
-import spray.json._
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -22,11 +21,10 @@ import scala.language.postfixOps
   * Get /api/car/X fetches cars with id x
   * Get /api/car/inventory?inStock=true
   */
-object HighLevelCarShopApi extends App with JsonSupport {
+object HighLevelCarShopApi extends App with JsonSupport with SprayJsonSupport {
   implicit val system: ActorSystem = ActorSystem("CarShop")
   implicit val materializer: ActorMaterializer.type = ActorMaterializer
   implicit val timeOut: Timeout = Timeout(3 seconds)
-  import system.dispatcher
 
   val dbActor = dataSetup(system)
 
@@ -34,39 +32,18 @@ object HighLevelCarShopApi extends App with JsonSupport {
     (pathPrefix("api" / "car") & get) {
       path("inventory") {
         parameter(Symbol("inStock").as[Boolean]) { condition =>
-          complete(
-            (dbActor ? IsInStock(condition))
-              .mapTo[Seq[Car]]
-              .map(_.toJson.prettyPrint)
-              .map(toHttpEntity)
-          )
+          complete((dbActor ? IsInStock(condition)).mapTo[Seq[Car]])
         }
       } ~
         (path(IntNumber) | parameter(Symbol("id").as[Int])) { id =>
-          complete(
-            (dbActor ? FindCarsById(id))
-              .mapTo[Option[Car]]
-              .map(_.toJson.prettyPrint)
-              .map(toHttpEntity)
-          )
+          complete((dbActor ? FindCarsById(id)).mapTo[Option[Car]])
         } ~
         pathEndOrSingleSlash {
-          complete(
-            (dbActor ? FindAllCars)
-              .mapTo[Seq[Car]]
-              .map(_.toJson.prettyPrint)
-              .map(toHttpEntity)
-          )
+          complete((dbActor ? FindAllCars).mapTo[Seq[Car]])
         }
     }
 
-  Http().newServerAt("localhost", 8080).bindFlow(carServerRoute)
-
-  private def toHttpEntity(content: String) =
-    HttpEntity(
-      ContentTypes.`application/json`,
-      content
-    )
+  Http().newServerAt("localhost", 8081).bindFlow(carServerRoute)
 }
 
 object CarShop {
